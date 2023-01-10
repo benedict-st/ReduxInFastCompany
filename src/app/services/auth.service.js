08e73b6a74abca571a9482e131cf178e7061fe89
Valya <benedict.1@yandex.ru>
2023-01-10 21:45:54 +0300

    Initial commit

--- src/app/services/auth.service.js
+++ src/app/services/auth.service.js
@@ -4,7 +4,7 @@ import localStorageService from "./localStorage.service";
 const httpAuth = axios.create({
     baseURL: "https://identitytoolkit.googleapis.com/v1/",
     params: {
-        key: ""
+        key: process.env.REACT_APP_FIREBASE_KEY
     }
 });
 
